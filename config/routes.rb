239a3deddc9f80ba583c905bb188d67e7be2e664
Tomoyuki Kurosawa <1717271+7coAim@users.noreply.github.com>
2018-05-06 14:57:35 +0900

    お問い合わせメール機能をローカル環境で実装

--- config/routes.rb
+++ config/routes.rb
@@ -15,5 +15,10 @@ Rails.application.routes.draw do
   resources :sessions, only: [:new, :create, :destroy]
 
   resources :favorites, only: [:create, :destroy]
-  
+
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  end
+
+
 end
