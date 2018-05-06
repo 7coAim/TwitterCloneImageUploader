748a83c5c6b81e0620fe88fbce4ab4e8787591c0
Tomoyuki Kurosawa <1717271+7coAim@users.noreply.github.com>
2018-05-06 15:33:05 +0900

    メール送信先の変更

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,6 +1,6 @@
 class ContactMailer < ApplicationMailer
   def contact_mail(contact)
     @contact = contact
-    mail to: 'support@twitterclone.com', subject: '【お問い合わせ受付完了】お問い合わせいただき、ありがとうございます。'
+    mail to: @contact.email, subject: '【お問い合わせ受付完了】お問い合わせいただき、ありがとうございます。'
   end
 end
